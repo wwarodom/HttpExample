a663257a90dba20f0058f3eaf9407bebc368f44d
Warodom Werapun <wwarodom@gmail.com>
2016-10-02 11:05:15 +0700

    Hide Server IP

--- app/src/main/java/com/warodom/httpexample/ClientFacade.java
+++ app/src/main/java/com/warodom/httpexample/ClientFacade.java
@@ -2,21 +2,18 @@ package com.warodom.httpexample;
 
 import com.loopj.android.http.*;
 
-/**
- * Created by wwaro on 30/9/2559.
- */
 
-public class ClientFacade {
+class ClientFacade {
 
-    private static final String BASE_URL = "http://172.26.1.108/laravel/www/public/";
+    private static final String BASE_URL = "http://Server_IP/laravel/www/public/";
 
     private static AsyncHttpClient client = new AsyncHttpClient();
 
-    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+    static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
-    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+    static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
